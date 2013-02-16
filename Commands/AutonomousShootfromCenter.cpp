c4a40f41fe79fc51bd62a89aa0bd1a4342dc58a6
Owen Woods <e.g.o.713@gmail.com>
2013-02-16 15:35:48 -0500

    Expiremental autonomous stuff

--- Commands/AutonomousShootfromCenter.cpp
+++ Commands/AutonomousShootfromCenter.cpp
@@ -27,10 +27,14 @@ AutonomousShootfromCenter::AutonomousShootfromCenter()
 	// Command1 and Command2 will run in parallel.
 
 	// ramp the shooter speed up
-	AddSequential(new SetShooterSpeed(3000));
+	AddParallel(new SetShooterSpeed(3000));
 	
 	// set the shooter angle
-	AddParallel(new SetShooterAngle(585));
+	AddSequential(new SetShooterAngle(585));
+	
+	// wait
+	AddSequential(new WaitCommand(AUTONOMOUS_SHOOT_WAIT1));
+	//AddSequential(new WaitForChildren(2));
 	
 	// shoot 1st frisbee
     AddSequential(new FrisbeeShoot());
