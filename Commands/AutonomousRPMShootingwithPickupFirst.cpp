8ebd527aa911f73fac998a20304d759b6baf51f6
Mike Goin <mike.goin12@gmail.com>
2013-03-29 16:13:29 -0400

    Changed pickup first to not drive back

--- Commands/AutonomousRPMShootingwithPickupFirst.cpp
+++ Commands/AutonomousRPMShootingwithPickupFirst.cpp
@@ -34,7 +34,8 @@ AutonomousRPMShootingwithPickupFirst::AutonomousRPMShootingwithPickupFirst()
 
    // ensure the ultrasonic range finder indicates a correct position
    // drive straight to get the Frisbees
-   AddSequential(new ChassisDrivetoWallStraight(DRIVE_UNDER_TOWER_DISTANCE, DRIVE_UNDER_TOWER_POWER), 3.0);
+   //AddSequential(new ChassisDrivetoWallStraight(DRIVE_UNDER_TOWER_DISTANCE, DRIVE_UNDER_TOWER_POWER), 3.0);
+   AddSequential(new ChassisDriveDistanceStraight(DRIVE_UNDER_TOWER_DURATION, DRIVE_UNDER_TOWER_POWER), 3.0);
    //AddSequential(new ChassisSafeMoveForward(), 3.0);
 
    // wait to allow the robot to move under the tower
@@ -90,5 +91,5 @@ AutonomousRPMShootingwithPickupFirst::AutonomousRPMShootingwithPickupFirst()
    AddSequential(new SetShooterAngle(SHOOTER_ANGLE_FOR_PICKUP), 2.0);
    
    // back up from tower
-   AddSequential(new ChassisDriveDistanceStraight(1.0, -0.8), 3.0);
+   //AddSequential(new ChassisDriveDistanceStraight(1.0, -0.8), 3.0);
 }
